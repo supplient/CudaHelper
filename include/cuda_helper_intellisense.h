8f8336febc1c28e7ae7f9340e5e716329b8b5a8e
supplient <437566830@qq.com>
2021-11-07 13:28:41 +0800

    添加cub的intellisense hint

--- include/cuda_helper_intellisense.h
+++ include/cuda_helper_intellisense.h
@@ -2,6 +2,7 @@
 
 // ========== Only For Intellisense =============
 
+// Kernels
 #ifdef __INTELLISENSE__
 	// ref: https://stackoverflow.com/a/27992604/17132546
 	#define KERNEL_ARGS(args) 
@@ -15,4 +16,15 @@
 	#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
 	#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
 
+#endif
+
+
+// CUB
+#ifdef __INTELLISENSE__
+	#define CUB_WRAP(x)
+#else
+	#define CUB_WRAP(x) x
+
+	// include will also cause intellisense error, so I include cub file here
+	#include <cub/cub.cuh>
 #endif
